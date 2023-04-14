a059f309ca96e065bfb61bc35ae2de98383981b0
Yasunaga <sys04jmwrx@docomonet.jp>
2023-04-14 18:09:26 +0900

    add 20230414

--- envs/prod/log/app_foobar/backend.tf
+++ envs/prod/log/app_foobar/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "yasu-tfstate"
-    key    = "yasu/prod/app/foobar_v1.0.0.tfstate"
+    key = "yasu/prod/log/app_foobar_v1.0.0.tfstate"
     region = "ap-northeast-1"
   }
 }
